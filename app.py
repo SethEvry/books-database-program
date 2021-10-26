6846cb7a5b52dcd6a1939251a3dff4c39e37b227
SethEvry <92412142+SethEvry@users.noreply.github.com>
2021-10-26 17:26:11 -0400

    add view books

--- app.py
+++ app.py
@@ -96,7 +96,9 @@ def app():
             print('Book Added!')
             time.sleep(1.5)
         elif choice == "2":  # View all books
-            pass
+            for book in session.query(Book):
+                print(f'{book.id} | {book.title} | {book.author}')
+            input("\nPress enter to return to the main menu.")
         elif choice == "3":  # Search for a book
             pass
         elif choice == "4":  # Book Analysis
