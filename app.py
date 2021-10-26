64f033167f2a7112da1279bb0d65df88ec326f20
SethEvry <92412142+SethEvry@users.noreply.github.com>
2021-10-26 16:31:53 -0400

    create menu

--- app.py
+++ app.py
@@ -1,5 +1,26 @@
 from models import (Base, session, Book, engine)
 
 
+def menu():
+    while True:
+        print('''
+              \nProgramming Books
+              \r1) Add book
+              \r2) View all books
+              \r3) Seach for book
+              \r4) Book Analysis
+              \r5) Exit
+              ''')
+        choice = input('What would you like to do? ')
+        if choice in ['1', '2', '3', '4', '5']:
+            return choice
+        else:
+            input('''
+                  \rPlease choose one of the options above.
+                  \rA number from 1-5.
+                  \rPress enter to try again.''')
+
+
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
+    menu()
